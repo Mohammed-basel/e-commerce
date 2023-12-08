589055d7abeaa47edcbce99ffb21e941c61b7732
mohammed basel <mohammedbasel410@gmail.com>
2023-12-08 16:18:46 +0200

    error fix

--- src/components/web/categories/Categories.jsx
+++ src/components/web/categories/Categories.jsx
@@ -4,19 +4,27 @@ import { useQuery } from "react-query";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Link } from "react-router-dom";
-import './categories.css'
+import "./categories.css";
 
 export default function Categories() {
   const getCategories = async () => {
-    const { data } = await axios.get(
-      `${import.meta.env.VITE_API_URL}/categories`
-    );
-
-    return data;
+    try {
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/categories`);
+      return data;
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+      throw new Error("Failed to fetch categories");
+    }
   };
-  const { data, isLoading } = useQuery("web_categories", getCategories);
+
+  const { data, isLoading, isError } = useQuery("web_categories", getCategories);
+
   if (isLoading) {
     return <p>Loading....</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading categories</p>;
   }
 
   return (
@@ -27,18 +35,20 @@ export default function Categories() {
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        {data?.categories.length
-          ? data?.categories.map((category) => (
-              <SwiperSlide key={category._id}>
-                <Link to={`products/category/${category._id}`}>
-                  <div className="category">
-                <img src={category.image.secure_url} className="rounded-circle" />
-                <h2>{category.name}</h2>
+        {data?.categories.length ? (
+          data.categories.map((category) => (
+            <SwiperSlide key={category._id}>
+              <Link to={`products/category/${category._id}`}>
+                <div className="category">
+                  <img src={category.image.secure_url} alt={category.name} className="rounded-circle" />
+                  <h2>{category.name}</h2>
                 </div>
-                </Link>
-              </SwiperSlide>
-            ))
-          : <h2>no category found</h2>}
+              </Link>
+            </SwiperSlide>
+          ))
+        ) : (
+          <h2>No categories found</h2>
+        )}
       </Swiper>
     </div>
   );
